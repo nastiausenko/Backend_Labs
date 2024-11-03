99e722d70afa8eba83d7e84dab8d02aeb6f2a873
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-11-03 14:18:04 +0200

    init commit

---
+++ src/test/java/dev/usenkonastia/backend_lab2/BackendLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.usenkonastia.backend_lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
