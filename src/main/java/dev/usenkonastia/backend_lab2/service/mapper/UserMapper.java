0499503d42704bbd1d6b71a4ecc583a68dd87c10
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-12-22 11:59:41 +0200

    add login and register functionality

--- src/main/java/dev/usenkonastia/backend_lab2/service/mapper/UserMapper.java
+++ src/main/java/dev/usenkonastia/backend_lab2/service/mapper/UserMapper.java
@@ -1,9 +1,10 @@
 package dev.usenkonastia.backend_lab2.service.mapper;
 
 
-import dev.usenkonastia.backend_lab2.domain.UserDetails;
+import dev.usenkonastia.backend_lab2.domain.User;
 import dev.usenkonastia.backend_lab2.dto.user.UserEntryDto;
 import dev.usenkonastia.backend_lab2.dto.user.UserListDto;
+import dev.usenkonastia.backend_lab2.dto.user.request.LoginRequestDto;
 import dev.usenkonastia.backend_lab2.dto.user.request.RegisterRequestDto;
 import dev.usenkonastia.backend_lab2.entity.UserEntity;
 import org.mapstruct.Mapper;
@@ -20,35 +21,42 @@ public interface UserMapper {
     @Mapping(source = "name", target = "name")
     @Mapping(source = "email", target = "email")
     @Mapping(source = "password", target = "password")
-    UserDetails toUser(UserEntity userEntity);
+    User toUser(UserEntity userEntity);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(source = "username", target = "name")
     @Mapping(source = "email", target = "email")
     @Mapping(source = "password", target = "password")
     @Mapping(target = "categories", ignore = true)
-    UserDetails toUser(RegisterRequestDto registerRequestDto);
+    User toUser(RegisterRequestDto registerRequestDto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "name", ignore = true)
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "password", target = "password")
+    @Mapping(target = "categories", ignore = true)
+    User toUser(LoginRequestDto loginRequestDto);
 
     @Mapping(source = "id", target = "id")
     @Mapping(source = "name", target = "name")
     @Mapping(source = "email", target = "email")
     @Mapping(source = "password", target = "password")
     @Mapping(source = "categories", target = "categories")
-    UserEntity toUserEntity(UserDetails user);
+    UserEntity toUserEntity(User user);
 
     @Mapping(source = "name", target = "username")
     @Mapping(source = "email", target = "email")
     @Mapping(source = "password", target = "password")
-    RegisterRequestDto toRegisterUserDto(UserDetails userDetails);
+    RegisterRequestDto toRegisterUserDto(User user);
 
-    default UserListDto toUserListDto(List<UserDetails> userDetails) {
+    default UserListDto toUserListDto(List<User> userDetails) {
         return UserListDto.builder().users(toUserEntry(userDetails)).build();
     }
 
-    List<UserEntryDto> toUserEntry(List<UserDetails> users);
+    List<UserEntryDto> toUserEntry(List<User> users);
 
-    default List<UserDetails> toUserList(Iterator<UserEntity> userServiceEntityIterator) {
-        List<UserDetails> result = new ArrayList<>();
+    default List<User> toUserList(Iterator<UserEntity> userServiceEntityIterator) {
+        List<User> result = new ArrayList<>();
         userServiceEntityIterator.forEachRemaining(
                 (cosmoCatEntity) -> {
                     result.add(toUser(cosmoCatEntity));
